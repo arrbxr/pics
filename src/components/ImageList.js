cf1024c1dab3072b8662b59b3274764d27c90abb
Abhishek Raj Ravi <arr.bxr@gmail.com>
2020-01-09 20:50:11 +0000

    implement dynamic span

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -3,7 +3,7 @@ import React from "react";
 import ImageCard from "./ImageCard";
 
 const ImageList = ({ images }) => {
-  const image = images.map(image => <ImageCard key={image.id} image={image} />);
+  const image = images.map(image => <ImageCard style={{border: "1px solid red"}} key={image.id} image={image} />);
   return <div className="image-list">{image}</div>;
 };
 
